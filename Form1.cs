cc164799d3e95a19b6857bf7cdf28ef3b726b7b4
Lenovo-User <Lenovo-User@DESKTOP-KGOC38Q>
2024-01-26 17:52:36 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tshepang_Tlhoaele_assessment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string newWord = textBox1.Text.Trim();
+            listBox1.Items.Add(newWord);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string newWord = textBox1.Text.Trim();
+            listBox1.Items[listBox1.SelectedIndex] = newWord;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBox1.Text.Clone();
+        }
+    }
+}
